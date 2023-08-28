838543ad2b4e7013e5bd7684cae9c99eaf57d0f8
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-28 14:27:56 -0700

    added createBook module

--- createBook.js
+++ createBook.js
@@ -1,1 +1,22 @@
 'use strict';
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+const BookModel = require('./BookModel.js');
+const MONGODB_URL = process.env.MONGODB_URL;
+
+mongoose.connect(`${MONGODB_URL}/can-of-books-cd-frontend`);
+
+let book = new BookModel({
+  title: '',
+  description: '',
+  status: '',
+});
+
+book.save().then(document => {
+  console.log(document);
+
+  BookModel.find({}).then(documents => {
+    console.log(documents);
+  });
+});
